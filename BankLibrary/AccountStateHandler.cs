6eb35472c6eb143c5bbe1c0cf47d8b25e07b1415
iMakarosha <40589438+iMakarosha@users.noreply.github.com>
2018-06-26 11:30:13 +0400

    Добавьте файлы проекта.

---
+++ BankLibrary/AccountStateHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankLibrary
+{
+    //делегат для создания событий
+    public delegate void AccountStateHandler(object sender, AccountEventArgs e);
+
+    //класс для обработки событий
+    public class AccountEventArgs
+    {
+        //message
+        public string Message { get; private set; }
+        //сумма изменения счета
+        public decimal Sum { get; private set; }
+
+        public AccountEventArgs(string _mes, decimal _sum)
+        {
+            Message = _mes;
+            Sum = _sum;
+        }
+    }
+
+    public abstract class Account : IAccount
+    {
+        //событие при выводе денег
+        protected internal event AccountStateHandler Withdrawed;
+        //событие при добавлении на счет
+    }
+}
---
+++ BankLibrary/IAccount.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankLibrary
+{
+    interface IAccount
+    {
+        //положить деньги на счет
+        void Put(decimal sum);
+        //снять со счета 
+        decimal Withdraw(decimal sum);
+    }
+}
